5058b81a91da93dd94dcecbc4ff0fee88135956a
Gilad Molek <gsmolek@gmail.com>
2021-08-23 21:54:34 +0300

    Added main function, added color change of blocks and edited bellman equation function

--- src/QLearning.py
+++ src/QLearning.py
@@ -10,9 +10,8 @@ import random
 
 class QLearning:
     def __init__(self, alpha, gamma, epsilon, number_of_rows, number_of_columns, number_of_actions):
-        board_size = number_of_columns * number_of_rows
-        print("board:" + str(board_size))
-        self.q_table = np.zeros((board_size, number_of_actions+1))
+        self.board_size = (number_of_columns) * (number_of_rows)
+        self.q_table = np.zeros((self.board_size, number_of_actions+1))
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
@@ -26,7 +25,6 @@ class QLearning:
         for i in range(0, self.number_of_rows):
             for j in range(0, self.number_of_columns ):
                 array.append( self.number_of_columns * i + j)
-        print(len(array))
         i = 0
         for x in array:
             self.q_table[i][0] = x
@@ -37,20 +35,38 @@ class QLearning:
             step = random.randint(0, self.number_of_actions -1)
             return step
         else:
-            step = np.argmax(self.q_table[state][1:3])
+            step = np.argmax(self.q_table[state][1:5])
             actions_available =[]
-            if self.q_table[state][1] == step :
+            if self.q_table[state][1] == self.q_table[state][step+1] :
                 actions_available.append(0)
-            if self.q_table[state][2] == step :
+            if self.q_table[state][2] == self.q_table[state][step+1] :
                 actions_available.append(1)
-            if self.q_table[state][3] == step :
+            if self.q_table[state][3] == self.q_table[state][step+1] :
                 actions_available.append(2)
-            if self.q_table[state][4] == step :
+            if self.q_table[state][4] == self.q_table[state][step+1] :
                 actions_available.append(3)
             return random.choice(actions_available)
     
+    def exploit_second_best(self,state):
+        step = self.q_table[state][1:5]
+        temp = self.q_table[state][1:5]
+        actions_available =[]
+        np.sort(temp)
+        if temp[1] == step[0]:
+            actions_available.append(0)
+        if temp[1] == step[1]:
+            actions_available.append(1)
+        if temp[1] == step[2]:
+            actions_available.append(2)
+        if temp[1] == step[3]:
+            actions_available.append(3)
+        return random.choice(actions_available)
+    
     def bellman_equation(self,state,new_state,action,reward):
-        self.q_table[state][action + 1] = self.q_table[state][action + 1] + self.alpha * (reward + self.gamma * np.max(self.q_table[new_state][1:4]) - self.q_table[state][action+1])
+        if state<0 or state >= self.board_size or new_state<0 or new_state >= self.board_size:
+            self.q_table[state][action + 1] = self.q_table[state][action + 1] + self.alpha * (reward + self.gamma * (-1000) - self.q_table[state][action+1])
+        else:
+            self.q_table[state][action + 1] = self.q_table[state][action + 1] + self.alpha * (reward + self.gamma * np.max(self.q_table[new_state][1:4]) - self.q_table[state][action+1])
         
     def print_table(self):
         print(self.q_table)
